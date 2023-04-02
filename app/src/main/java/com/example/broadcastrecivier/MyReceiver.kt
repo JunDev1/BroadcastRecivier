e1d6fa4f2aba5936c8af74217fe18c09c9c7647a
JunDev1 <86519269+JunDev1@users.noreply.github.com>
2023-04-02 22:42:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/broadcastrecivier/MyReceiver.kt
@@ -0,0 +1,24 @@
+package com.example.broadcastrecivier
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when (intent?.action) {
+            Intent.ACTION_BATTERY_LOW -> {
+                Toast.makeText(context, "Battery is low", Toast.LENGTH_SHORT).show()
+            }
+            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
+                val turnedOn = intent.getBooleanExtra("state", false)
+                Toast.makeText(
+                    context,
+                    "Airplane mod changed. Turned on $turnedOn",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+}
