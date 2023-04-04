0a6d947ac60d4ccca859f37ba0ec8102b98d657d
JunDev1 <86519269+JunDev1@users.noreply.github.com>
2023-04-04 18:12:46 +0300

    lesson 12.4

--- app/src/main/java/com/example/broadcastrecivier/MyReceiver.kt
+++ app/src/main/java/com/example/broadcastrecivier/MyReceiver.kt
@@ -8,6 +8,16 @@ import android.widget.Toast
 class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         when (intent?.action) {
+            "loaded" -> {
+                val percent = intent.getIntExtra("percent", 0)
+                Toast.makeText(context, "Loaded $percent", Toast.LENGTH_SHORT).show()
+
+            }
+            ACTION_CLICK -> {
+                val count = intent.getIntExtra(EXTRA_COUNT, 0)
+                Toast.makeText(context, "Clicked $count", Toast.LENGTH_SHORT).show()
+
+            }
             Intent.ACTION_BATTERY_LOW -> {
                 Toast.makeText(context, "Battery is low", Toast.LENGTH_SHORT).show()
             }
@@ -20,5 +30,10 @@ class MyReceiver : BroadcastReceiver() {
                 ).show()
             }
         }
+    }
+
+    companion object {
+        const val ACTION_CLICK = "clicked"
+        const val EXTRA_COUNT = "count"
     }
 }
